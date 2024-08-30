90d0f12686bced7734a1480e7caf6996ab4f5cb2
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2024-08-31 00:41:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+exports.pagination = function(data=[] , page=1 , limit=10) {
+
+    if (limit >= data.length-1 || limit <= 0) {
+        limit = 10;
+        page = 1;
+    }
+
+    let upperLimit = page * limit - 1;
+    let lowerLimit = (page - 1) * limit;
+
+    let arr;
+
+    if (lowerLimit < 0) {
+        arr = [];
+    } else if (lowerLimit > data.length-1) {
+        arr = [];
+    } else if (lowerLimit <= data.length-1) {
+        arr = data.slice(lowerLimit , upperLimit+1);
+    }
+
+    return arr;
+}
+
+// console.log(pagination(data , 3 , 5));
+
+// module.exports.pagination = pagination;
